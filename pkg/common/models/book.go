2bdcd9aaef11a3e2c86bd0157764466e55705f00
Kevin <hellokevinvogel@gmail.com>
2022-04-05 18:50:33 +0200

    fix(): book id

--- pkg/common/models/book.go
+++ pkg/common/models/book.go
@@ -1,7 +1,7 @@
 package models
 
 type Book struct {
-	Id          string `json:"id"`
+	Id          int    `json:"id" gorm:"primaryKey"`
 	Title       string `json:"title"`
 	Author      string `json:"author"`
 	Description string `json:"description"`
